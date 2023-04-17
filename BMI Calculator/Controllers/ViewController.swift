6b05926a57b090c3b6b7f944688a8355cfd08867
jeny3g <jeny3g@icloud.com>
2023-04-17 18:54:22 -0300

    feat: :sparkles: iboutlet and ibaction for slider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+        
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let weight = Int(sender.value)
+        weightLabel.text! = "\(weight)Kg"
+    }
+    
 }
 
