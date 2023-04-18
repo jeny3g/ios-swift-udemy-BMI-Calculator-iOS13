33c28dc74deea43bca5d37b32fb236b621474998
jeny3g <jeny3g@icloud.com>
2023-04-18 14:46:18 -0300

    feat: :sparkles: segues and navigation for multi-screen apps

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
+    
+    var bmiValue: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,8 +39,19 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / pow(height, 2)
+        bmiValue = String(format: "%.1f", bmi)
         
-        print(bmi)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
+
+    }
+    
 }
 
